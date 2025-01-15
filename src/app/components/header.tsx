1b5073e7d40d1ad8e0c7905da210384192902242
Luiz Henrique Fernandes da Silva <91839356+luiz-henrique-f@users.noreply.github.com>
2025-01-15 00:57:52 -0300

    settings for calendar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,15 @@
 function Header() {
   return (
-    <div className="h-[150px] text-center bg-teal-800">
-        <h1 className="pt-5 text-white font-black text-2xl">
-           Controle Financeiro 
+    <>
+      <div className="h-[150px] bg-teal-800">
+        <h1 className="text-white font-black text-2xl px-3">
+          {'< 01/2024 >'}
         </h1>
-    </div>
+        <h1 className="text-white font-black text-2xl text-center">
+          Controle Financeiro
+        </h1>
+      </div>
+    </>
   );
 }
 
